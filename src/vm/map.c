48dfac07ed247006a81dd65881324cbdfad06039
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2018-06-18 19:05:07 +0200

    Decreased size of map_entry_t pool

--- src/vm/map.c
+++ src/vm/map.c
@@ -969,7 +969,7 @@ int _map_init(vm_map_t *kmap, vm_object_t *kernel, void **bss, void **top)
 	vm_pageGetStats(&freesz);
 
 	/* Init map entry pool */
-	map_common.nfree = map_common.ntotal = freesz / (2 * SIZE_PAGE);
+	map_common.nfree = map_common.ntotal = freesz / (4 * SIZE_PAGE + sizeof(map_entry_t));
 
 	while ((*top) - (*bss) < sizeof(map_entry_t) * map_common.ntotal)
 		_page_sbrk(&map_common.kmap->pmap, bss, top);
