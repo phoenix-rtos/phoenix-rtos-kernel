7328b2c6ced60413a5b96c6b5a87357644e4793b
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2018-07-10 11:58:26 +0200

    arm-imx: Compilation and warnings fix

--- src/programs.c
+++ src/programs.c
@@ -86,8 +86,8 @@ int programs_decode(vm_map_t *kmap, vm_object_t *kernel)
 		hal_memcpy(vaddr, cpio, fs);
 //		vm_munmap(kmap, vaddr, sz);
 
-		pr->start = (u64)vaddr;
-		pr->end = (u64)vaddr + fs;
+		pr->start = (typeof(pr->start))vaddr;
+		pr->end = (typeof(pr->end))vaddr + fs;
 
 		cpio = (void *)cpio + fs;
 	}
