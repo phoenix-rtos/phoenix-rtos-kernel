8c64546dbd810b1df831b8bfbe7e286bfce94837
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2019-11-13 13:53:47 +0100

    imxrt: Cleanup, increased ustack size

--- src/hal/armv7/cpu.h
+++ src/hal/armv7/cpu.h
@@ -20,9 +20,8 @@
 
 #define SIZE_PAGE       0x200
 
-#define SIZE_USTACK     (2 * SIZE_PAGE)
-
 #ifdef CPU_IMXRT
+#define SIZE_USTACK     (4 * SIZE_PAGE)
 #define SIZE_KSTACK     (4 * 512)
 #define RET_HANDLER_MSP 0xffffffe1
 #define RET_THREAD_MSP  0xffffffe9
@@ -30,6 +29,7 @@
 #define HWCTXSIZE       (8 + 18)
 #define USERCONTROL     0x7
 #else
+#define SIZE_USTACK     (2 * SIZE_PAGE)
 #define SIZE_KSTACK     (2 * 512)
 #define RET_HANDLER_MSP 0xfffffff1
 #define RET_THREAD_MSP  0xfffffff9
