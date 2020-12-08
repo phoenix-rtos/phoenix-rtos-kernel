8e9de96516152738e0245319788344ae7c659fd2
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2020-12-08 17:28:04 +0100

    process: launch process from syspage

--- proc/process.h
+++ proc/process.h
@@ -89,6 +89,9 @@ extern int proc_start(void (*initthr)(void *), void *arg, const char *path);
 extern int proc_fileSpawn(const char *path, char **argv, char **envp);
 
 
+extern int proc_syspageSpawnName(const char *map, const char *name, char **argv);
+
+
 extern int proc_syspageSpawn(syspage_program_t *program, vm_map_t *map, const char *path, char **argv);
 
 
