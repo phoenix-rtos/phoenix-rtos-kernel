111012a7e332c3d29a2670163c8e7402035f2060
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-09-07 13:41:28 +0200

    hal/armv7m: share hal.h between all platforms
    and:
     - remove syspage.* from hal/armv7m,
     - remove ifdefs from hal.h,
     - platform specfic headers are located in config.h,
    
    JIRA: RTOS-91, RTOS-92

--- hal/armv7m/hal.c
+++ hal/armv7m/hal.c
@@ -13,18 +13,25 @@
  * %LICENSE%
  */
 
-#include "syspage.h"
-#include "spinlock.h"
-#include "console.h"
-#include "interrupts.h"
-#include "spinlock.h"
-#include "timer.h"
-#include "cpu.h"
-
-
+#include "../hal.h"
 struct {
 	int started;
 } hal_common;
+
+
+hal_syspage_t *syspage;
+
+
+void *hal_syspageRelocate(void *data)
+{
+	return data;
+}
+
+
+ptr_t hal_syspageAddr(void)
+{
+	return (ptr_t)syspage;
+}
 
 
 int hal_started(void)
@@ -47,7 +54,6 @@ void _hal_init(void)
 	_hal_cpuInit();
 	_hal_consoleInit();
 	_timer_init(SYSTICK_INTERVAL);
-	_hal_syspageInit();
 
 	hal_common.started = 0;
 
