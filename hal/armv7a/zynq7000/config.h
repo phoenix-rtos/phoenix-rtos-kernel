21bbb4a03fc593db39405a407ba82c6307157f1b
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-09-20 10:37:29 +0200

    armv7a/zynq7000: move defines to config.h
    JIRA: PD-9

--- hal/armv7a/zynq7000/config.h
+++ hal/armv7a/zynq7000/config.h
@@ -16,6 +16,19 @@
 #ifndef _HAL_CONFIG_H_
 #define _HAL_CONFIG_H_
 
+/* TODO: temp solution, defines should be located in appropriate headers */
+#define SYSTICK_IRQ 42
+
+#define TIMER_US2CYC(x) ((55555LL * (x)) / 1000LL)
+#define TIMER_CYC2US(x) (((x) * 1000LL) / 55555LL)
+
+#define ADDR_DDR 0x00100000
+#define SIZE_DDR 0x7ffffff
+
+#ifndef __ASSEMBLY__
+
 #include "../../include/arch/syspage-zynq7000.h"
+
+#endif
 
 #endif
