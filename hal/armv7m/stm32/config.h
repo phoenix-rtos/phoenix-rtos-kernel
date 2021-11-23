6c069a1dca8c103ace7355faaeb0828cba46c4af
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-11-23 15:09:37 +0100

    hal/armv7m/stm32: add new syspage approach and share pmap with imxrt family
    JIRA: RTOS-131

---
+++ hal/armv7m/stm32/config.h
@@ -0,0 +1,22 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system kernel
+ *
+ * Configuration file for STM32
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Hubert Buczynski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _HAL_CONFIG_H_
+#define _HAL_CONFIG_H_
+
+#include "stm32.h"
+#include "../../include/arch/syspage-stm32.h"
+
+#endif
