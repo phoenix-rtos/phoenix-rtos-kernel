45e00616b4c2762f22399d51d038d58897804bc8
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2022-01-25 23:49:01 +0100

    posix: add ALLPERMS and ACCESSPERMS macros, clean up file type and permission macros
    JIRA: RTOS-148

--- include/posix.h
+++ include/posix.h
@@ -44,24 +44,56 @@ struct sockaddr {
 };
 
 
-#define S_IFMT   0170000
-#define S_IFIFO  0010000
-#define S_IFCHR  0020000
-#define S_IFDIR  0040000
-#define S_IFBLK  0060000
-#define S_IFREG  0100000
-#define S_IFLNK  0120000
-#define S_IFSOCK 0140000
+/* File type */
+#define S_IFMT   0xf000 /* File type mask */
+#define S_IFSOCK 0xc000 /* Socket */
+#define S_IFLNK  0xa000 /* Symbolic link */
+#define S_IFREG  0x8000 /* Regular file */
+#define S_IFBLK  0x6000 /* Block device */
+#define S_IFDIR  0x4000 /* Directory */
+#define S_IFCHR  0x2000 /* Character device */
+#define S_IFIFO  0x1000 /* FIFO */
 
-#define S_BLKSIZE 512
+#define S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK) /* Socket */
+#define S_ISLNK(m)  (((m) & S_IFMT) == S_IFLNK)  /* Symbolic link */
+#define S_ISREG(m)  (((m) & S_IFMT) == S_IFREG)  /* Regular file */
+#define S_ISBLK(m)  (((m) & S_IFMT) == S_IFBLK)  /* Block device */
+#define S_ISDIR(m)  (((m) & S_IFMT) == S_IFDIR)  /* Directory */
+#define S_ISCHR(m)  (((m) & S_IFMT) == S_IFCHR)  /* Character device */
+#define S_ISFIFO(m) (((m) & S_IFMT) == S_IFIFO)  /* FIFO */
 
-#define S_ISDIR(m)  (((m) & (0170000)) == 0040000)
-#define S_ISCHR(m)  (((m) & (0170000)) == 0020000)
-#define S_ISBLK(m)  (((m) & (0170000)) == 0060000)
-#define S_ISREG(m)  (((m) & (0170000)) == 0100000)
-#define S_ISFIFO(m) (((m) & (0170000)) == 0010000)
-#define S_ISLNK(m)  (((m) & (0170000)) == 0120000)
-#define S_ISSOCK(m) (((m) & (0170000)) == 0140000)
+/* File permissions */
+#define S_ISUID 0x0800 /* Set user ID on execution */
+#define S_ISGID 0x0400 /* Set group ID on execution */
+#define S_ISVTX 0x0200 /* Sticky bit */
+
+#define S_IRWXU 0x01c0 /* RWX mask for owner */
+#define S_IRUSR 0x0100 /* R for owner */
+#define S_IWUSR 0x0080 /* W for owner */
+#define S_IXUSR 0x0040 /* X for owner */
+
+#define S_IRWXG 0x0038 /* RWX mask for group */
+#define S_IRGRP 0x0020 /* R for group */
+#define S_IWGRP 0x0010 /* W for group */
+#define S_IXGRP 0x0008 /* X for group */
+
+#define S_IRWXO 0x0007 /* RWX mask for other */
+#define S_IROTH 0x0004 /* R for other */
+#define S_IWOTH 0x0002 /* W for other */
+#define S_IXOTH 0x0001 /* X for other */
+
+/* BSD compatibility macros */
+#define S_ISTXT  S_ISVTX
+#define S_IREAD  S_IRUSR
+#define S_IWRITE S_IWUSR
+#define S_IEXEC  S_IXUSR
+
+#define S_BLKSIZE 512 /* Block size (stat.st_blocks unit) */
+
+#define ALLPERMS    (S_ISUID | S_ISGID | S_ISVTX | S_IRWXU | S_IRWXG | S_IRWXO) /* 07777 */
+#define ACCESSPERMS (S_IRWXU | S_IRWXG | S_IRWXO)                               /* 0777 */
+#define DEFFILEMODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH) /* 0666 */
+
 
 #define SEEK_SET 0
 #define SEEK_CUR 1
