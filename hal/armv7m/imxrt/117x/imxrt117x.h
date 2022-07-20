b8dd23930df89f6c244136a3e934924628ba80b3
Gerard P.Swiderski <gerard.swiderski@phoenix-rtos.com>
2022-07-20 17:55:48 +0200

    imxrt117x: add lpcg platformctl
    JIRA: NIL-303

--- hal/armv7m/imxrt/117x/imxrt117x.h
+++ hal/armv7m/imxrt/117x/imxrt117x.h
@@ -5,7 +5,7 @@
  *
  * iMXRT basic peripherals control functions
  *
- * Copyright 2019 Phoenix Systems
+ * Copyright 2019-2022 Phoenix Systems
  * Author: Aleksander Kaminski
  *
  * This file is part of Phoenix-RTOS.
@@ -51,6 +51,15 @@ extern int _imxrt_setIOisel(int isel, char daisy);
 
 
 extern int _imxrt_setDevClock(int clock, int div, int mux, int mfd, int mfn, int state);
+
+
+extern int _imxrt_setDirectLPCG(int clock, int state);
+
+
+extern int _imxrt_getDirectLPCG(int clock, int *state);
+
+
+extern int _imxrt_setLevelLPCG(int clock, int level);
 
 
 extern void _imxrt_platformInit(void);
