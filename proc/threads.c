f4f1b617565b13dd1dcf817a79d2a53e7cb740f8
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2023-03-20 19:26:50 +0100

    proc/threads.c: fix ustack parameter passed in to hal_cpuCreateContext()
    JIRA: RTOS-407

--- proc/threads.c
+++ proc/threads.c
@@ -828,7 +828,7 @@ int proc_threadCreate(process_t *process, void (*start)(void *), unsigned int *i
 	t->lastTime = t->startTime;
 
 	/* Prepare initial stack */
-	hal_cpuCreateContext(&t->context, start, t->kstack, t->kstacksz, stack + stacksz, arg);
+	hal_cpuCreateContext(&t->context, start, t->kstack, t->kstacksz, (stack == NULL) ? NULL : (unsigned char *)stack + stacksz, arg);
 	threads_canaryInit(t, stack);
 
 	if (process != NULL && (process->tls.tdata_sz != 0 || process->tls.tbss_sz != 0)) {
