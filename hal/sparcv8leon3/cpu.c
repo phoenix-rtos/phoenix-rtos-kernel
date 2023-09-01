e15898bac82e168f7e6aaea60d413790a282f389
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2023-09-01 11:21:17 +0200

    sparcv8leon3/reboot: remove unnecessary register setting
    JIRA: RTOS-586

--- hal/sparcv8leon3/cpu.c
+++ hal/sparcv8leon3/cpu.c
@@ -22,19 +22,8 @@
 #include "config.h"
 
 
-#define GPTIMER0_BASE  0x80003000
-#define WATCHDOG_CTRL  0x78
-#define DIS_LVDS_ADDR  0x8000d030
-#define CGU_UNLOCK     0x80006000
-#define CGU_EN         0x80006004
-#define CGU_RESET      0x80006008
-#define BO_VMON        0x8010c018
 #define BOOTSTRAP_ADDR 0x80008000
 #define BOOTSTRAP_SPIM 0x400BC003
-#define TESTCFG_ADDR   0x8000E000
-#define TESTCFG_DISR   0x3
-#define SPIM_CTRL_ADDR 0xFFF00104
-#define SPIM_EDAC_ADDR 0xFFF00114
 
 
 #define STR(x)  #x
@@ -186,28 +175,6 @@ void hal_cpuReboot(void)
 {
 	/* Reset to the built-in bootloader */
 	hal_cpuDisableInterrupts();
-
-	/* Disable watchdog boot sequence */
-	*(volatile u32 *)(GPTIMER0_BASE + WATCHDOG_CTRL) = 0;
-
-	/* Clear Brownout */
-	*(volatile u32 *)(CGU_UNLOCK) = 0x8000000;
-	*(volatile u32 *)(CGU_EN) = 0x8000000;
-	*(volatile u32 *)(CGU_RESET) = 0;
-	*(volatile u32 *)(CGU_UNLOCK) = 0;
-
-	*(volatile u32 *)(BO_VMON) = 0x7F;
-	*(volatile u32 *)(BO_VMON) = 0;
-
-	/* Disable LVDS */
-	*(volatile u32 *)(DIS_LVDS_ADDR) = 0x00888888;
-
-	/* Disable watchdog reset */
-	*(volatile u32 *)(TESTCFG_ADDR) = TESTCFG_DISR;
-
-	/* Enable alt scaler and disable EDAC for SPI memory */
-	*(volatile u32 *)(SPIM_CTRL_ADDR) = 0x4;
-	*(volatile u32 *)(SPIM_EDAC_ADDR) = 0;
 
 	/* Reboot to SPIM */
 	*(volatile u32 *)(BOOTSTRAP_ADDR) = BOOTSTRAP_SPIM;
