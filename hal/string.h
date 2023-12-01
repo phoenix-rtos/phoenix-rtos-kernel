87ef7164eaa884ca1d761e072046c2922efad189
Andrzej Stalke <andrzej.stalke@phoenix-rtos.com>
2023-12-01 15:50:16 +0100

    hal: Make prefix const in hal_i2s
    JIRA: RTOS-679

--- hal/string.h
+++ hal/string.h
@@ -44,7 +44,7 @@ extern char *hal_strcpy(char *dest, const char *src);
 extern char *hal_strncpy(char *dest, const char *src, size_t n);
 
 
-extern unsigned long hal_i2s(char *prefix, char *s, unsigned long i, unsigned char b, char zero);
+extern unsigned long hal_i2s(const char *prefix, char *s, unsigned long i, unsigned char b, char zero);
 
 
 #endif
