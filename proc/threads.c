0a2a4037fd1b8a1911e8a7369ccf4ca405b3fd88
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-02-05 11:27:40 +0100

    !proc/threads: update `id` type in `threadCreate`
    JIRA: RTOS-125

--- proc/threads.c
+++ proc/threads.c
@@ -708,7 +708,7 @@ void threads_canaryInit(thread_t *t, void *ustack)
 }
 
 
-int proc_threadCreate(process_t *process, void (*start)(void *), unsigned int *id, unsigned int priority, size_t kstacksz, void *stack, size_t stacksz, void *arg)
+int proc_threadCreate(process_t *process, void (*start)(void *), int *id, unsigned int priority, size_t kstacksz, void *stack, size_t stacksz, void *arg)
 {
 	thread_t *t;
 	spinlock_ctx_t sc;
