91007668dab19f222d88d24f51e3c7e0959aa5c5
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-08-27 11:26:53 +0200

    rename sparcv8leon3 to sparcv8leon
    JIRA: RTOS-883

---
+++ include/arch/sparcv8leon/syspage.h
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system kernel
+ *
+ * HAL syspage for sparcv8leon
+ *
+ * Copyright 2023 Phoenix Systems
+ * Author: Lukasz Leczkowski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _PHOENIX_SYSPAGE_LEON3_H_
+#define _PHOENIX_SYSPAGE_LEON3_H_
+
+
+typedef struct {
+	int dummy;
+} __attribute__((packed)) hal_syspage_t;
+
+#endif
