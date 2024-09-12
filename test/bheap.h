889e50222d9b0823f99dfbe24e336745721c8993
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2024-09-12 11:55:41 +0200

    test: Add binary heap test
    JIRA: RTOS-917

---
+++ test/bheap.h
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system kernel
+ *
+ * Min/max binary heap test
+ *
+ * Copyright 2024 Phoenix Systems
+ * Author: Aleksander Kaminski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+
+#ifndef _TEST_BH_H_
+#define _TEST_BH_H_
+
+
+extern void test_bh(void);
+
+
+#endif
