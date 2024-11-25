a06c65a4d0007ca90052ead2363fa97139e0aa90
Adam Greloch <adam.greloch@phoenix-rtos.com>
2024-11-25 09:29:52 +0100

    hal/ia32: add pctl_pcicfg command
    Allows for controlling the state of various pci device configuration registers
    
    JIRA: RTOS-937

--- hal/ia32/pci.h
+++ hal/ia32/pci.h
@@ -5,8 +5,8 @@
  *
  * PCI driver
  *
- * Copyright 2019, 2020 Phoenix Systems
- * Author: Kamil Amanowicz, Lukasz Kosinski
+ * Copyright 2019, 2020, 2024 Phoenix Systems
+ * Author: Kamil Amanowicz, Lukasz Kosinski, Adam Greloch
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -19,13 +19,13 @@
 #include "include/arch/ia32/ia32.h"
 
 
-extern int hal_pciSetBusmaster(pci_dev_t *dev, u8 enable);
-
-
 extern int hal_pciSetUsbOwnership(pci_usbownership_t *usbownership);
 
 
 extern int hal_pciGetDevice(pci_id_t *id, pci_dev_t *dev, void *caps);
+
+
+extern int hal_pciSetConfigOption(pci_pcicfg_t *pcicfg);
 
 
 extern void _hal_pciInit(void);
