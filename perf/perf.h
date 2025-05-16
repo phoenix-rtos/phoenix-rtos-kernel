424d43f17e1211341b07a2eaa3ba5d036e503159
Adam Greloch <adam.greloch@phoenix-rtos.com>
2025-05-16 16:12:37 +0200

    !perf: introduce performance analysis subsystem
    Introduces a subsystem intended for various performance analysis
    mechanisms: tracing, sample-based methods, PMU utilization, etc.
    
    Breaking change: This commit generalizes the `perf_*` syscalls to
    support various methods described above. Previous perf semantics are
    preserved in the `perf_mode_threads` mode.
    
    The subsystem currently implements `perf_mode_trace` mode - a mechanism
    for tracing syscalls, interrupts, locks and scheduler events. The events
    are outputed in CTF format to a cbuffer that can be retrieved from
    userspace using `perf_read(perf_mode_trace)` and later processed by 3rd
    party libraries like babeltrace2.
    
    JIRA: RTOS-1057
    (cherry picked from commit d510a8fc69bf4cb95b8fd5ccd77bd352290fe1c9)

---
+++ perf/perf.h
@@ -0,0 +1,33 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system kernel
+ *
+ * Performance analysis subsystem
+ *
+ * Copyright 2025 Phoenix Systems
+ * Author: Adam Greloch
+ *
+ * %LICENSE%
+ */
+
+#ifndef _PERF_PERF_H_
+#define _PERF_PERF_H_
+
+#include "include/perf.h"
+#include "vm/map.h"
+
+
+extern int _perf_init(vm_map_t *kmap);
+
+
+extern int perf_start(perf_mode_t mode, unsigned pid);
+
+
+extern int perf_read(perf_mode_t mode, void *buf, size_t bufsz);
+
+
+extern int perf_finish(perf_mode_t mode);
+
+
+#endif
