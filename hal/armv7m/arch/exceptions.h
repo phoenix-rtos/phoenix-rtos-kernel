15acde72b412b348992fc57c1ef9b3dc1bd9d523
Jakub Klimek <jakub.klimek@phoenix-rtos.com>
2025-05-29 17:39:50 +0200

    hal/armv7m: coredump implementation
    JIRA: RTOS-1062

--- hal/armv7m/arch/exceptions.h
+++ hal/armv7m/arch/exceptions.h
@@ -8,8 +8,17 @@
 
 #define EXC_UNDEFINED 3
 
-#define SIZE_CTXDUMP 320 /* Size of dumped context */
+#define SIZE_CTXDUMP          320 /* Size of dumped context */
+#define SIZE_COREDUMP_GREGSET 72
+#ifdef CPU_IMXRT
+#define SIZE_COREDUMP_THREADAUX 280 /* vfp context note */
+#define SIZE_COREDUMP_GENAUX    36  /* auxv HWCAP note */
+#else
+#define SIZE_COREDUMP_THREADAUX 0
+#define SIZE_COREDUMP_GENAUX    0
+#endif
 
+#define HAL_ELF_MACHINE 40 /* ARM */
 
 typedef cpu_context_t exc_context_t;
 
