c12f88812b270955a84878002efeb059434f750b
Jakub Klimek <jakub.klimek@phoenix-rtos.com>
2025-08-06 10:54:14 +0200

    syscalls: don't mask signals that cannot be ignored
    POSIX requirement:
    It is not possible to block those signals which cannot be ignored. This
    shall be enforced by the system without causing an error to be indicated
    
    JIRA: RTOS-736

--- syscalls.c
+++ syscalls.c
@@ -1280,7 +1280,7 @@ int syscalls_signalPost(u8 *ustack)
 
 unsigned int syscalls_signalMask(u8 *ustack)
 {
-	unsigned int mask, mmask, old;
+	unsigned int mask, mmask, old, new;
 	thread_t *t;
 
 	GETFROMSTACK(ustack, unsigned int, mask, 0U);
@@ -1289,7 +1289,9 @@ unsigned int syscalls_signalMask(u8 *ustack)
 	t = proc_current();
 
 	old = t->sigmask;
-	t->sigmask = (mask & mmask) | (t->sigmask & ~mmask);
+	new = (mask & mmask) | (old & ~mmask);
+
+	threads_setSigmask(t, new);
 
 	return old;
 }
@@ -1316,7 +1318,7 @@ void syscalls_sigreturn(u8 *ustack)
 	hal_cpuDisableInterrupts();
 	hal_cpuSigreturn(t->kstack + t->kstacksz, ustack, &ctx);
 
-	t->sigmask = oldmask;
+	threads_setSigmask(t, oldmask);
 
 	/* TODO: check if return address belongs to user mapped memory */
 	if (hal_cpuSupervisorMode(ctx) != 0) {
