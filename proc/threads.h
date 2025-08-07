9ee8d65cf1ee97413b81a8ebce7e2424bc445e5e
Jakub Klimek <jakub.klimek@phoenix-rtos.com>
2025-08-07 12:27:20 +0200

    process: lazy allocation of sigactions
    Reduce size of process_t struct by allocating
    sigactions array only when needed.
    
    JIRA: RTOS-736

--- proc/threads.h
+++ proc/threads.h
@@ -227,4 +227,12 @@ __attribute__((noreturn)) void threads_halt(void);
 int threads_setSigaction(int sig, void (*trampoline)(void), const struct sigaction *act, struct sigaction *old);
 
 
+/* Requires guarantee that parent won't be reaped during execution. */
+int proc_cloneSigactions(process_t *parent, process_t *child);
+
+
+/* POSIX: signals ignored by the execve calling process should remain ignored */
+void proc_resetExecSigactions(void);
+
+
 #endif
