5332a7b5fbf160ad278d35c2fd05bc3befb28eb8
Adam Debek <adam.debek@phoenix-rtos.com>
2025-10-30 14:28:14 +0100

    include: add O_ACCMODE, O_RSYNC and O_DSYNC to fcntl.h
    JIRA: RTOS-1129

--- include/posix-fcntl.h
+++ include/posix-fcntl.h
@@ -19,18 +19,22 @@
 
 #define FD_CLOEXEC 1
 
-#define O_RDONLY   0x0001
-#define O_WRONLY   0x0002
-#define O_RDWR     0x0004
-#define O_APPEND   0x0008
-#define O_CREAT    0x0100
-#define O_TRUNC    0x0200
-#define O_EXCL     0x0400
-#define O_SYNC     0x0800
-#define O_NONBLOCK 0x1000
+#define O_RDONLY   0x00001
+#define O_WRONLY   0x00002
+#define O_RDWR     0x00004
+#define O_APPEND   0x00008
+#define O_CREAT    0x00100
+#define O_TRUNC    0x00200
+#define O_EXCL     0x00400
+#define O_SYNC     0x00800
+#define O_NONBLOCK 0x01000
 #define O_NDELAY   O_NONBLOCK
-#define O_NOCTTY   0x2000
-#define O_CLOEXEC  0x4000
+#define O_NOCTTY   0x02000
+#define O_CLOEXEC  0x04000
+#define O_RSYNC    0x08000
+#define O_DSYNC    0x10000
+
+#define O_ACCMODE (O_RDONLY | O_WRONLY | O_RDWR)
 
 /* clang-format off */
 
