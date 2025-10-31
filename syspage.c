5bb99131d10d53480b529fd207dfb99960f6dae2
Adam Greloch <adam.greloch@phoenix-rtos.com>
2025-10-31 17:34:30 +0100

    syspage: remove unnecessary suppresion

--- syspage.c
+++ syspage.c
@@ -16,12 +16,12 @@
 #include "lib/lib.h"
 #include "syspage.h"
 
-/* parasoft-begin-suppress MISRAC2012-RULE_8_4 "Global variable used in assembler code" */
+
 static struct {
 	/* parasoft-suppress-next-line MISRAC2012-RULE_5_8 "Variable inside the structure so it shouldn't cause this violation" */
 	syspage_t *syspage;
 } syspage_common;
-/* parasoft-end-suppress MISRAC2012-RULE_8_4 */
+
 
 size_t syspage_mapSize(void)
 {
