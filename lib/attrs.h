80c349303fc001e9c805d372d3d9b125cc934229
Adam Greloch <adam.greloch@phoenix-rtos.com>
2026-02-04 12:01:41 +0100

    lib: introduce MAYBE_UNUSED macro
    JIRA: DO-3

---
+++ lib/attrs.h
@@ -0,0 +1,21 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system kernel
+ *
+ * Common attribute macros
+ *
+ * Copyright 2025 Phoenix Systems
+ * Author: Adam Greloch
+ *
+ * %LICENSE%
+ */
+
+#ifndef _PH_LIB_ATTRS_H_
+#define _PH_LIB_ATTRS_H_
+
+
+#define MAYBE_UNUSED __attribute__((unused))
+
+
+#endif
