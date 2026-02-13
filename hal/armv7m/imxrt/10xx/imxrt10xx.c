e7bc5facb45ee47ec706dfbe22f081c294f07ea3
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-02-13 11:48:18 +0100

    hal/armv7m/imxrt/10xx: fix clk_pll_enet1 get bug
    JIRA: RTOS-1212

--- hal/armv7m/imxrt/10xx/imxrt10xx.c
+++ hal/armv7m/imxrt/10xx/imxrt10xx.c
@@ -1147,7 +1147,7 @@ u32 _imxrt_ccmGetPllFreq(int pll)
 			break;
 
 		case clk_pll_enet1:
-			divSel = *(imxrt_common.ccm_analog + ccm_analog_pll_enet) & (0x3U << 2);
+			divSel = (*(imxrt_common.ccm_analog + ccm_analog_pll_enet) >> 2) & (0x3U);
 
 			switch (divSel) {
 				case 0:
