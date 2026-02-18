cbfa5d037fda72c31c50b5ed9108012313856923
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-02-18 11:12:28 +0100

    posix: transfer common ioctl defines to include/ioctl
    Common defines in kernel to keep the ABI consistent
    
    JIRA: RTOS-1205

--- include/ioctl.h
+++ include/ioctl.h
@@ -16,6 +16,19 @@
 #ifndef _PH_IOCTL_H_
 #define _PH_IOCTL_H_
 
+#define IOCPARM_MASK   0x1fffUL
+#define IOCPARM_LEN(x) (((x) >> 16) & IOCPARM_MASK)
+#define IOCBASECMD(x)  ((x) & ~(IOCPARM_MASK << 16))
+#define IOCGROUP(x)    (((x) >> 8) & 0xffU)
+
+#define IOC_VOID    0x00000000UL
+#define IOC_NESTED  0x20000000UL
+#define IOC_OUT     0x40000000UL
+#define IOC_IN      0x80000000UL
+#define IOC_INOUT   (IOC_IN | IOC_OUT)
+#define IOC_DIRMASK 0xc0000000UL
+
+#define _IOC(inout, group, num, len) ((unsigned long)((inout) | (((len) & IOCPARM_MASK) << 16) | ((group) << 8) | (num)))
 
 typedef struct {
 	unsigned long request;
