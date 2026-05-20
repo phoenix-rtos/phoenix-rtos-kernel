b48a245f3017a4f75eb9a00d035c9dd9b4e81ff6
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2026-05-20 10:53:46 +0200

    hal/arm/spinlock: refactor for MISRA compliance
    Encapsulate assembly language into inline functions.
    Suppress automatic check warnings.
    
    JIRA: RTOS-1304

--- hal/arm/spinlock.c
+++ hal/arm/spinlock.c
@@ -31,21 +31,48 @@ static struct {
 } spinlock_common;
 
 
-void hal_spinlockSet(spinlock_t *spinlock, spinlock_ctx_t *sc)
+/* parasoft-begin-suppress MISRAC2012-DIR_4_3 "Assembly is required for low-level operations" */
+
+/* Mask IRQ and return previous mask state */
+static inline spinlock_ctx_t spinlock_maskIRQ(void)
 {
-	u32 tmp;
+	spinlock_ctx_t sc;
 	/* clang-format off */
 	__asm__ volatile (
 #if KERNEL_SPINLOCK_ARM_M
-		"mrs %[tmp], primask\n"
+		"mrs %[sc], primask\n"
 		"cpsid i\n"
 #else
-		"mrs %[tmp], cpsr\n"
+		"mrs %[sc], cpsr\n"
 		"cpsid if\n"
 #endif
-	: [tmp] "=r" (tmp));
-	*sc = tmp;
+	: [sc] "=r" (sc));
+	/* clang-format on */
+	return sc;
+}
 
+
+/* Restore previous IRQ mask state */
+static inline void spinlock_restoreIRQ(spinlock_ctx_t sc)
+{
+	/* clang-format off */
+	__asm__ volatile (
+#if KERNEL_SPINLOCK_ARM_M
+		"msr primask, %[sc]\n"
+#else
+		"msr cpsr_c, %[sc]\n"
+#endif
+	:
+	: [sc] "r" (sc));
+	/* clang-format on */
+}
+
+
+/* Acquire the spinlock's flag by atomically checking and setting it to 0. Wait indefinitely while flag is set to 0. */
+static inline void spinlock_acquireFlag(u8 *lock)
+{
+	u32 tmp;
+	/* clang-format off */
 	__asm__ volatile (
 	"1:\n"
 		"ldrexb %[tmp], [%[lock]]\n"
@@ -56,31 +83,39 @@ void hal_spinlockSet(spinlock_t *spinlock, spinlock_ctx_t *sc)
 		"bne 1b\n"
 		"dmb\n"
 	: [tmp] "=&r" (tmp) /* `tmp` cannot share register with `zero` or `lock` */
-	: [lock] "r" (&spinlock->lock), [zero] "r" (0)
+	: [lock] "r" (lock), [zero] "r" (0)
 	: "memory", "cc");
 	/* clang-format on */
 }
 
 
-void hal_spinlockClear(spinlock_t *spinlock, spinlock_ctx_t *sc)
+/* Release the spinlock's flag by atomically setting it to 1. */
+static inline void spinlock_releaseFlag(u8 *lock)
 {
 	/* clang-format off */
 	__asm__ volatile (
 		"dmb\n"
 		"strb %[one], [%[lock]]\n"
 	:
-	: [one] "r" (1), [lock] "r" (&spinlock->lock)
+	: [one] "r" (1), [lock] "r" (lock)
 	: "memory");
-
-	__asm__ volatile (
-#if KERNEL_SPINLOCK_ARM_M
-		"msr primask, %[tmp]\n"
-#else
-		"msr cpsr_c, %[tmp]\n"
-#endif
-	:
-	: [tmp] "r" (*sc));
 	/* clang-format on */
+}
+
+/* parasoft-end-suppress MISRAC2012-DIR_4_3 */
+
+
+void hal_spinlockSet(spinlock_t *spinlock, spinlock_ctx_t *sc)
+{
+	*sc = spinlock_maskIRQ();
+	spinlock_acquireFlag(&spinlock->lock);
+}
+
+
+void hal_spinlockClear(spinlock_t *spinlock, spinlock_ctx_t *sc)
+{
+	spinlock_releaseFlag(&spinlock->lock);
+	spinlock_restoreIRQ(*sc);
 }
 
 
