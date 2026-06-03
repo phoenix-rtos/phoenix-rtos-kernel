9756f8886da76e33fd50f4adab7178049bbe188a
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-06-03 16:49:53 +0200

    lib/cbuffer: improve const-correctness
    YT: RTOS-1343

--- lib/cbuffer.h
+++ lib/cbuffer.h
@@ -28,7 +28,7 @@ typedef struct {
 } cbuffer_t;
 
 
-static inline size_t _cbuffer_free(cbuffer_t *buf)
+static inline size_t _cbuffer_free(const cbuffer_t *buf)
 {
 	if (buf->w == buf->r) {
 		return (buf->full != 0U) ? 0U : buf->sz;
@@ -37,7 +37,7 @@ static inline size_t _cbuffer_free(cbuffer_t *buf)
 }
 
 
-static inline size_t _cbuffer_avail(cbuffer_t *buf)
+static inline size_t _cbuffer_avail(const cbuffer_t *buf)
 {
 	return buf->sz - _cbuffer_free(buf);
 }
