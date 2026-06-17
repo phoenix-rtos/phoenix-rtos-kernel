1ddeed82da0eb51a864180e3cf4a9528c97dedeb
Adam Greloch <adam.greloch@phoenix-rtos.com>
2026-06-17 18:16:42 +0200

    proc: add schedInfo syscall
    TASK: RTOS-1353

--- include/sched.h
+++ include/sched.h
@@ -14,10 +14,21 @@
 #ifndef _PH_SCHED_H_
 #define _PH_SCHED_H_
 
+#include "types.h"
 
 #define SCHED_FIFO  0
 #define SCHED_RR    1
 #define SCHED_OTHER 2
+
+
+typedef struct {
+	time_t interval;
+	int minPriority;
+	int maxPriority;
+	union {
+		char reserved[32]; /* reserved for policy-specific values */
+	} policy;
+} sched_info_t;
 
 
 #endif
