017b4986eddd04c145613a9a00ae137fad1219d3
Adam Greloch <adam.greloch@phoenix-rtos.com>
2026-06-17 18:15:26 +0200

    include/sched.h: move sched policy defines to the kernel
    TASK: RTOS-1353

---
+++ include/sched.h
@@ -0,0 +1,23 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Operating system kernel
+ *
+ * Scheduler definitions
+ *
+ * Copyright 2026 Phoenix Systems
+ * Authors: Adam Greloch
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _PH_SCHED_H_
+#define _PH_SCHED_H_
+
+
+#define SCHED_FIFO  0
+#define SCHED_RR    1
+#define SCHED_OTHER 2
+
+
+#endif
