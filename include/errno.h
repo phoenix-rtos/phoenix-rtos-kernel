2fadf92a820b8e54bd4e7ae185ea761233c93aa6
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2026-07-29 15:29:28 +0200

    !include/errno: transfer all errno defines from libphoenix
    It seems that we will need all of those definitions in the kernel at
    some point, so transfer all of them at once now so that they are
    available for use later and no more breaking commits than necessary
    need to be made.
    
    YT: RTOS-1408

--- include/errno.h
+++ include/errno.h
@@ -58,19 +58,47 @@
 #define EDEADLK      38
 #define EWOULDBLOCK  EAGAIN /* Operation would block */
 
+#define ENOTEMPTY 39
+#define ELOOP     40
+#define ENOMSG    42
+#define EUNATCH   49
+#define ENODATA   61
+#define ENONET    64
+#define EBADFD    66
+#define EPROTO    71
+#define EBADMSG   74
+#define EOVERFLOW 75
+#define EILSEQ    84
+
 #define ENOTSOCK        88
+#define EDESTADDRREQ    89
 #define EMSGSIZE        90
 #define EPROTOTYPE      91
 #define ENOPROTOOPT     92
 #define EPROTONOSUPPORT 93
 #define EOPNOTSUPP      95
+#define EPFNOSUPPORT    96
 #define EAFNOSUPPORT    97
 #define EADDRINUSE      98
+#define EADDRNOTAVAIL   99
+#define ENETDOWN        100
+#define ENETUNREACH     101
+#define ENETRESET       102
+#define ECONNABORTED    103
+#define ECONNRESET      104
+#define ENOBUFS         105
 #define EISCONN         106
 #define ENOTCONN        107
 #define ETIMEDOUT       110
 #define ECONNREFUSED    111
+#define EHOSTDOWN       112
+#define EHOSTUNREACH    113
 #define EALREADY        114
 #define EINPROGRESS     115
+#define ENOLCK          116
+#define EUCLEAN         117
+#define ENOTRECOVERABLE 131
+#define ENOTSUP         EOPNOTSUPP
+
 
 #endif
