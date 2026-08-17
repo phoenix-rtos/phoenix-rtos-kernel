91accab875a08ae83a833df9f22857c098629f8a
Adam Greloch <adam.greloch@phoenix-rtos.com>
2026-08-17 14:55:43 +0200

    !proc: increase the number of priorities from 8 to 64
    This change moves the priority range from [0,7] to [-32,31].
    
    This change is breaking:
    1. The priority had to be made signed.
    2. The priority syscall has been renamed to sys_priority.
    3. As a consequence of 1., the API of sys_priority and mutexPrioCeiling
       syscalls was modified to return current priority and previous
       priority ceiling, respectively, through an extra argument instead of
       the retval. Otherwise legal negative priority values would conflict
       with errors.
    
    [-32,31] range instead of [0,63] was chosen, as the existing userspace
    code is concentrated in the high prio range (<4) with 4 usually being
    the default thread priority. Expanding the range to negative values is a
    way to relax that contention without the need to reconfigure/rescale the
    priorities in the userspace code.
    
    If needed, the readyBitmask can be extended with an L2 bitmask to
    support more priorities.
    
    If needed, NPRIOS can be set to lower even values if memory is a
    concern.
    
    TASK: RTOS-1434

--- proc/threads.h
+++ proc/threads.h
@@ -22,13 +22,27 @@
 #include "process.h"
 #include "lock.h"
 
+#include <board_config.h>
+
 #ifdef DEBUG_THREADS
 #define LIB_ASSERT_THREADS(condition, fmt, ...) LIB_ASSERT_ALWAYS((condition), (fmt), ##__VA_ARGS__)
 #else
 #define LIB_ASSERT_THREADS(condition, fmt, ...)
 #endif
 
-#define MAX_PRIO 7U /* Maximum priority value, of the lowest criticality (prio=0 is of the HIGHEST) */
+#ifndef NPRIOS
+#define NPRIOS 64U
+#endif
+
+_Static_assert(NPRIOS % 2U == 0U, "NPRIOS should be even");
+_Static_assert(NPRIOS >= 16U, "NPRIOS should be greater than 16");
+
+typedef s8 priority_t;
+_Static_assert(NPRIOS <= (1UL << (sizeof(priority_t) * 8U)), "NPRIOS must fit into priority_t range");
+
+#define PRIO_OFFSET (NPRIOS / 2U)
+#define MAX_PRIO    (((priority_t)PRIO_OFFSET) - 1) /* Maximum priority value, of the lowest criticality (scheduled when no threads with p < MAX_PRIO are ready) */
+#define MIN_PRIO    (-((priority_t)PRIO_OFFSET))    /* Minimum priority value, of the HIGHEST criticality (scheduled before MIN_PRIO + 1) */
 
 #define MAX_TID        MAX_ID
 #define THREAD_END     1U
@@ -57,11 +71,12 @@ typedef struct _thread_t {
 	struct _thread_t **wait;
 	time_t wakeup;
 
-	unsigned int priorityBase : 4;
-	unsigned int priority : 4;
+	priority_t priorityBase;
+	priority_t priority;
+
 	unsigned int state : 2;
 	unsigned int exit : 2;
-	unsigned interruptible : 1;
+	unsigned int interruptible : 1;
 
 	unsigned int sigmask;
 	unsigned int sigpend;
@@ -100,10 +115,11 @@ thread_t *proc_current(void);
 void threads_canaryInit(thread_t *t, void *ustack);
 
 
-int proc_threadCreate(process_t *process, startFn_t start, int *id, u8 priority, size_t kstacksz, void *stack, size_t stacksz, unsigned int sigmask, void *arg);
+int proc_threadCreate(process_t *process, startFn_t start, int *id, priority_t priority, size_t kstacksz, void *stack, size_t stacksz, unsigned int sigmask, void *arg);
 
 
-int proc_threadPriority(thread_t *t, int signedPriority);
+/* Sets the priority of t to val or retrieves the current priority of t when val == PH_GET_PRIO. Returns the current priority of t in *res if res != NULL. */
+int proc_threadPriority(thread_t *t, int val, int *res);
 
 
 __attribute__((noreturn)) void proc_threadEnd(void);
@@ -190,7 +206,7 @@ int proc_settime(time_t offs);
 __attribute__((noreturn)) void proc_longjmp(cpu_context_t *ctx);
 
 
-void proc_threadsDump(u8 priority);
+void proc_threadsDump(priority_t priority);
 
 
 int _threads_init(vm_map_t *kmap, vm_object_t *kernel);
