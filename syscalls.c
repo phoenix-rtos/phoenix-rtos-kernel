68dbb8d17ab14d6c5b95b1f61228ab491ad4ae34
Jakub Klimek <jakub.klimek@phoenix-rtos.com>
2026-09-10 17:16:41 +0200

    syscalls: document potential additional arguments
    TASK: RTOS-736

--- syscalls.c
+++ syscalls.c
@@ -1275,6 +1275,7 @@ void syscalls_sigreturn(u8 *ustack)
 
 	GETFROMSTACK(ustack, unsigned int, oldmask, 0U);
 	GETFROMSTACK(ustack, cpu_context_t *, ctx, 1U);
+	/* NOTE: `hal_cpuSigreturn` may take additional arguments from ustack */
 
 	hal_cpuDisableInterrupts();
 	hal_cpuSigreturn(t->kstack + t->kstacksz, ustack, &ctx);
@@ -1301,6 +1302,7 @@ int syscalls_sys_open(u8 *ustack)
 
 	GETFROMSTACK(ustack, const char *, filename, 0U);
 	GETFROMSTACK(ustack, int, oflag, 1U);
+	/* NOTE: `posix_open` may take additional arguments from ustack */
 
 	return posix_open(filename, oflag, ustack);
 }
@@ -1452,6 +1454,7 @@ int syscalls_sys_fcntl(u8 *ustack)
 
 	GETFROMSTACK(ustack, int, fd, 0U);
 	GETFROMSTACK(ustack, unsigned int, cmd, 1U);
+	/* NOTE: `posix_fcntl` may take additional arguments from ustack */
 
 	return posix_fcntl(fd, cmd, ustack);
 }
@@ -1974,6 +1977,7 @@ int syscalls_sys_ioctl(u8 *ustack)
 
 	GETFROMSTACK(ustack, int, fildes, 0U);
 	GETFROMSTACK(ustack, unsigned long, request, 1U);
+	/* NOTE: `posix_ioctl` may take additional arguments from ustack */
 
 	/* vm_mapBelongs on optional data pointer checked in posix_ioctl */
 	return posix_ioctl(fildes, request, ustack);
